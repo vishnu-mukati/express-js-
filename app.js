29eeaee359bbaee4f1b1a285426ff23f357f08e7
vishnu-mukati <mukativishnu1232@gmaila.com>
2025-08-13 21:26:33 +0530

    express add

--- app.js
+++ app.js
@@ -1,38 +1,19 @@
-const http = require('http');
-const express = require('express');
-
+const express = require("express");
 const app = express();
 
-// app.use((req,res,next)=>{
-//     console.log('output from middleware 1');
-//     next();
-// })
 
-app.use((req,res,next)=>{
-    console.log('Authentication middleware called');
+function addUserMiddleware(req, res, next) {
+    req.user = "Guest"; 
     next();
-})
+}
 
-app.use("/library-2",(req,res,next)=>{
-    console.log("book recommendations");
-    next();    
-})
 
-app.use("library-3",(req,res,next)=>{
-    console.log("special access to research paper from professors and seniors");
-    next();
-})
+app.get("/welcome", addUserMiddleware, (req, res) => {
+    res.send(`<h1>Welcome, ${req.user}!</h1>`);
+});
 
-app.get("/library-2",(req,res)=>{
-    res.send("<h1>library 2 entered</h1>")
-})
 
-app.get((req,res)=>{
-    res.send("<h1>library 3 Entered</h1>")
-})
-
-app.listen(3000);
-
-// const server = http.createServer(app);
-
-// server.listen(3000);
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
